b7d9e2b764dd577cade51e63f6e4ac9678af4634
Sunny <Sunny@FUBEUSBNU2>
2019-01-28 19:06:39 +0530

    third change

--- jenkins_integration/src/main/java/jenkins_integration/Example.java
+++ jenkins_integration/src/main/java/jenkins_integration/Example.java
@@ -5,8 +5,7 @@ public class Example {
 	public static void main(String[] args) {
 		System.out.println("I am sunny");
 		System.out.println("This is my 1st");
-		System.out.println("This is my 2st");
-
+		System.out.println("This is my third build");
 	}
 
 }
